537a904812aab9684c8edfadd3e056ef9478e3a0
Vitaliy Zalutskiy <zal.vitali@gmail.com>
2023-01-30 15:38:45 +0300

    005 magic

---
+++ 005/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Clear();
+
+int count = 0;
+
+int xa = 40;
+int ya = 1;
+
+int xb = 2;
+int yb = 40;
+
+int xc = 80;
+int yc = 40;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+while (count < 10000)
+{
+    int what = new Random().Next(0, 3);
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
+
