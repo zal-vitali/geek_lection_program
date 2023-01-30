7c27be37e72efab1f258f13f4187f6ef14dda3a9
Vitaliy Zalutskiy <zal.vitali@gmail.com>
2023-01-30 13:20:36 +0300

    modify 002

--- 002/Program.cs
+++ 002/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Input number 1");
+double number1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Input number 2");
+double number2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine(number1 / number2);
