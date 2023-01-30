5bb5d9b88f61b32cac1a6f328a06b4091fad4778
Vitaliy Zalutskiy <zal.vitali@gmail.com>
2023-01-30 13:48:49 +0300

    add 004

---
+++ 004/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя"!);
+string Username = Console.ReadLine();
+if (Username.ToLower() == "маша")
+{
+    Console.Write("Ура, это же ");
+    Console.WriteLine(Username);
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(Username);
+}
