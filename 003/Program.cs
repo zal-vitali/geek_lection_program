aef8390d31e6188db5a7a6c171bc4e2abf653965
Vitaliy Zalutskiy <zal.vitali@gmail.com>
2023-01-30 13:29:50 +0300

    create 003

---
+++ 003/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1,10);
+int numberB = new Random().Next(1,10);
+int result = numberA + numberB;
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(result);
