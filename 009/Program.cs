9ac4ab3ee9b47603a91a7267324ed998130a613d
Vitaliy Zalutskiy <zal.vitali@gmail.com>
2023-02-17 10:25:18 +0300

    06_09

--- 009/Program.cs
+++ 009/Program.cs
@@ -1,9 +1,15 @@
-﻿double Fibonacci(int n)
+﻿
+double Fibonacci(int n, int q)
 {
+    q++;
     if (n == 1 || n == 2) return 1;
-    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+    else return Fibonacci(n - 1, q) + Fibonacci(n - 2, q);
 }
+
+int q = 0;
+
 for (int i = 1; i < 40; i++)
 {
-    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+    Console.WriteLine($"f({i}) = {Fibonacci(i,q)}");
 }
+
